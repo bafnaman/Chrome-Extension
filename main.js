e566f55370304558b4e05faa04281d088c8b86a6
bafnaman <70760458+bafnaman@users.noreply.github.com>
2021-08-04 18:30:14 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+let btn = document.getElementById("getTimezone");
+
+if(btn !==null){
+    btn.addEventListener("click",Timezone);
+}
+
+function Timezone(){
+    let time = document.getElementById("showTimezone");
+    time.innerHTML = Date();
+}
